6e3cc475d0cc0a91fa31d42a38cd9f8b75a95467
clydeee03 <144906657+clydeee03@users.noreply.github.com>
2023-09-13 19:51:50 +0800

    Add files via upload
    Sorry sir hehehe

---
+++ CS112_First Activity_Belongilot.py
@@ -0,0 +1,14 @@
+# CS112: Computer Programming 1
+print("Laboratory Exercise 1")
+name = "Clyde Joshua C.Belongilot"
+print("My name is", name)
+course = "BS Computer Science"
+print("I'm currently enrolled as a student in USTP taking", course)
+section = "CS1E"
+print("i am from section", section)
+age = "20 years old"
+print("I am", age)
+birthday = "August 5,2003"
+print("i was born on", birthday)
+address = "P-5 Topaz st. Gusa,CDO"
+print("I currently reside in ", address)
